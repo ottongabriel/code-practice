7f5d140a8a268699abab3e4de31b74035721e7e5
Otton G Fernandez <ottongabriel@MacBook-Air.local>
2018-06-16 21:51:10 -0400

    flatten function, now with more reduce

--- jun-16-1018/flatten.js
+++ jun-16-1018/flatten.js
@@ -2,16 +2,29 @@ let multiDArray = [[1],[2,[3],4],5];
 
 console.log(multiDArray);
 
+// function flattenArray(theArr){
+//   let flattenedArr = [];
+//   for(let i = 0; i < theArr.length; i++){
+//     if(Array.isArray(theArr[i])){
+//       flattenedArr.push(...flattenArray(theArr[i]));
+//     }
+//     else{
+//       flattenedArr.push(theArr[i]);
+//     }
+//   }
+//   return flattenedArr;
+// }
+
 function flattenArray(theArr){
-  let flattenedArr = [];
-  for(let i = 0; i < theArr.length; i++){
-    if(Array.isArray(theArr[i])){
-      flattenedArr.push(...flattenArray(theArr[i]));
+  let flattenedArr = theArr.reduce((accumulator, currentValue) => {
+    if(Array.isArray(currentValue)){
+      return accumulator.concat(...flattenArray(currentValue));
     }
     else{
-      flattenedArr.push(theArr[i]);
+      return accumulator.concat(currentValue);
     }
-  }
+
+  }, []);
   return flattenedArr;
 }
 
