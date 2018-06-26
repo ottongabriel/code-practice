bbdabbd21a15d20c6003cf04b68b044fbcdf3a0e
Otton G Fernandez <ottongabriel@MacBook-Air-4.local>
2018-06-25 20:01:28 -0400

    rename files, added comments

--- jun-24-2018/find-middle-number.js
+++ jun-24-2018/find-middle-number.js
@@ -1,4 +1,12 @@
+/*
+// Receives an array with exactly 3 integers. 
+// Assume that the are all different, but they are not necessarily in order. 
+// Returns the index of the middle number such that:
+// input -> [32, 4, 16]
+// output -> 2
+*/
 var gimme = function(inputArray) {
   return inputArray.indexOf([...inputArray].sort((a, b) => a - b)[1]);
 };
 
+// THE BEST SOLUTION WAS ALMOST EXACTLY THE SAME
